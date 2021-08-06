3b5d0d550aade5a6070b3c9a87d380a587e76af9
Favour Samuel <sfchidimma@gmail.com>
2021-08-06 19:33:45 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Double Click Heart
+Project #29 in Brad Traversy's 50 Projects in 50 Days. By clicking any part of the image, a heart pops up and it shows how many times it has been clicked.
+
+![Double Click Heart](screenshot.jpg)
+
+## Built With
+- [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
+- [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- [Javascript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Install
+```
+    $ git clone git@github.com:samiiexx/sam-js-29.git
+    $ cd main
+```  
+## Contributors
+- Favour Samuel (@samiiexx)
+
+## License
+Copyright (c) 2021 Favour Samuel.
+
+For enquiries please contact me at [sfchidimma@gmail.com](mailto:sfchidimma@gmail.com).
---
+++ index.js
@@ -0,0 +1,60 @@
+// Copyright 2021 sfchi
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const loveMe = document.querySelector('.loveMe')
+const times = document.getElementById('times')
+
+let clickTime = 0
+let timesClicked = 0
+
+loveMe.addEventListener('click', (e) => {
+    if(clickTime === 0) {
+        clickTime = new Date().getTime()
+    }
+    else {
+        if((new Date().getTime() - clickTime) < 800) {
+            createHeart(e)
+            clickTime = 0
+        }
+        else {
+            clickTime = new Date().getTime()
+        }
+    }
+})
+
+const createHeart = (e) => {
+    const heart = document.createElement('i')
+    heart.classList.add('fas')
+    heart.classList.add('fa-heart')
+
+    const x = e.clientX
+    const y = e.clientY
+
+    const leftOffset = e.target.offsetLeft
+    const topOffset = e.target.offsetTop
+
+    const xInside = x - leftOffset
+    const yInside = y - topOffset
+
+    heart.style.top = `${yInside}px`
+    heart.style.left = `${xInside}px`
+
+    loveMe.appendChild(heart)
+
+    times.innerHTML = ++timesClicked
+
+    setTimeout(() => {
+        heart.remove()
+    }, 2000);
+}
